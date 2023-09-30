ead568728b7bdf6616c117bfe6ebe6cd0242e2ec
skc52 <99289560+skc52@users.noreply.github.com>
2023-09-30 15:38:25 -0400

    added function to validate and parse position. validation is not complete

--- Board.lisp
+++ Board.lisp
@@ -68,4 +68,109 @@
     )
 )
 
-(print (update-board '((0 1 2) (3 4 5)) 0 2 5))
+;; function to ask for user input on the position
+(defun ask-user-position-input ()
+    (format t "Enter the position where you want to put your piece. Follow the format A10, K1, etc~%")
+    (let ((user-choice (read-line)))
+        (format t "You entered ~a" user-choice)           
+    
+
+        ;; check if input position is valid
+        (cond 
+            ((not (validate-user-input user-choice))
+                (ask-user-position-input)
+            )
+        )
+    )
+
+)
+
+;; function to validate position input
+;; must contain alpha and number, followng the format alpha then num
+;; number must be 1<num<19
+;; alphabets must be A<a<S
+
+(defun validate-user-input (user-input)
+   (let (
+        (input-length (length user-input))
+        (col-label (char user-input 0))
+        (row-label (subseq user-input 1)))
+
+   
+    ;;  if invalid length
+    (cond 
+        ((> input-length 3)
+            (print "Input Length cannot be more than 3. ")
+            (terpri)
+            ()
+        )
+        
+        (t 
+        ;; if invalid row
+            (cond 
+                ;; TODO check if the row input is strictly numeric
+                ;; Col input validation is fine
+
+
+                ((not (<= 1 (parse-integer row-label) 19))
+                    (print "Invalid row input.")
+                    (terpri)
+                    ;; return nil
+                    ()
+                )
+                (t 
+                    ;; if invalid column
+                    (cond 
+                        ((not (or (<= (char-code #\a) (char-code col-label)  (char-code #\s)) (<= (char-code #\A) (char-code col-label)  (char-code #\S)) ))
+                            (print "Invalid Column Input.")
+                            (terpri)
+                            ;; return nil
+                            ()
+                        )
+
+                        (t 
+                            t
+                        )  
+                    
+                    )
+
+                )
+            )
+
+        )
+    )
+    
+   )    
+)
+
+
+(defun parse-position (position-input)
+    ;; determine row label and column label
+    ;; convert both to indexing i and j
+    (let (
+        (col-label (char position-input 0))
+        (row-label (subseq position-input 1))
+        
+        )   
+
+        ;; if col in between A to S
+        ;; if col in between a to s
+
+        (cond 
+            ((<= (char-code #\a) (char-code col-label)  (char-code #\s))
+                ;; assign (char-code col-label)-(char-code #\a) to i ;; convert row to numeric type 
+                (append (list (+ (- (char-code col-label) (char-code #\a)) 1)) (list (parse-integer row-label)))
+            )
+            (t
+                ;; assign (char-code col-label)-(char-code #\a) to i ;; convert row to numeric type 
+                (append (list (+ (- (char-code col-label) (char-code #\A)) 1)) (list (parse-integer row-label)))
+            )
+        )     
+    )
+)
+
+;; (validate-user-input "A10")
+
+;; (ask-user-position-input)
+
+;; (print (update-board '((0 1 2) (3 4 5)) 0 2 5))
